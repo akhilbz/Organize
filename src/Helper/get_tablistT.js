ee4cbe59441d4c40784c831e4aed51dc0c80919e
akhilbz <akhilesh.bitla@gmail.com>
2023-05-22 12:55:18 -0500

    Implemented code to handle cases of enabling disabled group button state when closing certain tabs for properl functionality og the group button disabled feature

--- src/Helper/get_tablistT.js
+++ src/Helper/get_tablistT.js
@@ -10,9 +10,9 @@ function GetTabListForDT({tabType, currGroups, setCurrGroups, currGroupTabs, set
         tabType.map((tab, index) => {
         const title = tab.title.includes("-") ? tab.title.split("-")[0].trim() : tab.title.includes("–") ? tab.title.split("–")[0].trim() :
         tab.title.includes("|") ? tab.title.split("|")[0].trim() : tab.title;
-        const tab_hosturl = new URL(tab.url).hostname;
+        // const tab_hosturl = new URL(tab.url).hostname;
         const curr_tab = tab;
-        
+        // console.log(tab_hosturl);
         // Group Name Logic (group indicator):
         var group_name = "";
         if (tab.groupId != -1) {
@@ -35,34 +35,73 @@ function GetTabListForDT({tabType, currGroups, setCurrGroups, currGroupTabs, set
               <div className="tab-list-items d-flex">
               <h6 className="group-indicator">{group_name}</h6>
               <button type="button" className="btn-close" aria-label="Close" onClick={(event) => {
+                const tab_hosturl = new URL(tab.url).hostname;
+                console.log(tab_hosturl);
+                console.log("currTabs");
+                console.log(updatedTabs);
+                console.log("isGroupButtonDisabled");
+                console.log(isGroupButtonDisabled);
                 chrome.tabs.remove(tab.id);
                   const updatedGroupTabs = [];
                   const updatedTabs = currTabs.filter((remtab) => remtab.id != tab.id);
-
+                  console.log("updatedTabs");
+                  console.log(updatedTabs);
                   setCurrTabs([...updatedTabs]);
                   var updatedGroupsForTabs = [];
                   for (const grouped_arr of currGroupTabs) {
-                  
-                  const updated_arr = grouped_arr.filter((grouped_tab) => grouped_tab.id != tab.id);
-                  if (updated_arr.length > 0) {
-                    updatedGroupTabs.push(updated_arr);
-                  }
-                  }
-                
-                  if (tabType.length == 1) {
-                  var url_index = 0;
-                  var updatedGroupButtonDisabledArr = [];
-
-                  const updatedHostUrls = hostUrls.filter((url) => url != tab_hosturl);
-                  for (const url of hostUrls) {
-                    if (url != tab_hosturl) {
-                      updatedGroupButtonDisabledArr.push(isGroupButtonDisabled[url_index])
+                    const updated_arr = grouped_arr.filter((grouped_tab) => grouped_tab.id != tab.id);
+                    if (updated_arr.length > 0) {
+                      updatedGroupTabs.push(updated_arr);
                     }
-                    url_index++;
                   }
-                  setHostUrls([...updatedHostUrls]);
-                  setGroupButtonDisabled(updatedGroupButtonDisabledArr);
+
+                  if (tabType.length == 1) {
+                    const updatedHostUrls = hostUrls.filter((url) => url != tab_hosturl);
+                    var url_index = 0;
+                    var updatedGroupButtonDisabledArr = [];
+                    for (const url of hostUrls) {
+                      if (url != tab_hosturl) {
+                        updatedGroupButtonDisabledArr.push(isGroupButtonDisabled[url_index])
+                      }
+                      url_index++;
+                    }
+                    setHostUrls([...updatedHostUrls]);
+                    setGroupButtonDisabled(updatedGroupButtonDisabledArr);
+                  } else {
+                    var isButtonDisabled = [];
+                    var url_index = 0;
+                    for (const url of hostUrls) {
+                      if (tab_hosturl == url) {
+                        break;
+                      }
+                      url_index++;
+                    }
+                    var isDisabled = false;
+                    for (const hostTab of tabType) {
+                      if (hostTab != tab) {
+                        if (hostTab.groupId !== -1) {
+                          isDisabled = true;
+                        } else {
+                          isDisabled = false;
+                          break;
+                        }
+                      }
+                    }
+                    var updatedGroupButtonDisabledArr = [];
+                    var index = 0;
+
+                    for (const disabledVal of isGroupButtonDisabled) {
+                      if (index == url_index) {
+                        updatedGroupButtonDisabledArr.push(isDisabled);
+                      } else {
+                        updatedGroupButtonDisabledArr.push(disabledVal);
+                      }
+                      index++;
+                    }
+                    console.log(updatedGroupButtonDisabledArr);
+                    setGroupButtonDisabled([...updatedGroupButtonDisabledArr]);
                   }
+
                   if (currGroupTabs.length - updatedGroupTabs.length == 1) {
                   const updatedGroups = currGroups.filter((currGroup) => tab.groupId !== currGroup.id);
                   setCurrGroups(updatedGroups);
